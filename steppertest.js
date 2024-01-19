0657c65ca0ca948c827667b24f74a10cc886d07b
Mate Lenkey <9825717+lmate@users.noreply.github.com>
2024-01-19 17:20:10 +0100

    Stepper driver

--- steppertest.js
+++ steppertest.js
@@ -1,26 +1,18 @@
-#!/usr/bin/python3
-//import RPi.GPIO as GPIO
 import { Gpio } from 'onoff';
-//import time
 
-//in1 = 17
-//in2 = 18
-//in3 = 27
-//in4 = 22
 const in1 = new Gpio(17, 'out');
 const in2 = new Gpio(18, 'out');
 const in3 = new Gpio(27, 'out');
 const in4 = new Gpio(22, 'out');
 
-// careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
-const step_sleep = 0.002;
+const step_sleep = 1;
 
-const step_count = 4096; // 5.625*(1/64) per step, 4096 steps is 360°
+const step_count = 4096;
 
-const direction = False; // True for clockwise, False for counter-clockwise
+const direction = false;
 
-// defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
-const step_sequence = [[1, 0, 0, 1],
+const step_sequence = [
+[1, 0, 0, 1],
 [1, 0, 0, 0],
 [1, 1, 0, 0],
 [0, 1, 0, 0],
@@ -29,56 +21,24 @@ const step_sequence = [[1, 0, 0, 1],
 [0, 0, 1, 1],
 [0, 0, 0, 1]];
 
-// setting up
-//GPIO.setmode( GPIO.BCM )
-//GPIO.setup( in1, GPIO.OUT )
-//GPIO.setup( in2, GPIO.OUT )
-//GPIO.setup( in3, GPIO.OUT )
-//GPIO.setup( in4, GPIO.OUT )
-
-// initializing
-//GPIO.output( in1, GPIO.LOW )
-//GPIO.output( in2, GPIO.LOW )
-//GPIO.output( in3, GPIO.LOW )
-//GPIO.output( in4, GPIO.LOW )
-
-const motor_pins = [in1, in2, in3, in4];
 let motor_step_counter = 0;
 
 function cleanup() {
-  //GPIO.output( in1, GPIO.LOW )
-  //GPIO.output( in2, GPIO.LOW )
-  //GPIO.output( in3, GPIO.LOW )
-  //GPIO.output( in4, GPIO.LOW )
-  //GPIO.cleanup();
   in1.writeSync(0);
   in2.writeSync(0);
   in3.writeSync(0);
   in4.writeSync(0);
-  LED.unexport();
+  in1.unexport();
+  in2.unexport();
+  in3.unexport();
+  in4.unexport();
 }
 
-// the meat
-/*
-try:
-    i = 0
-    for i in range(step_count):
-        for pin in range(0, len(motor_pins)):
-            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
-        if direction==True:
-            motor_step_counter = (motor_step_counter - 1) % 8
-        elif direction==False:
-            motor_step_counter = (motor_step_counter + 1) % 8
-        else: # defensive programming
-            print( "uh oh... direction should *always* be either True or False" )
-            cleanup()
-            exit( 1 )
-        time.sleep( step_sleep )
-*/
 function run() {
   for (let i = 0; i < step_count; i++) {
-    setTimeout(step, step_sleep * i);
+    setTimeout(step, step_sleep * (i + 1));
   }
+  setTimeout(cleanup, step_sleep * (step_count + 1));
 }
 
 function step() {
@@ -95,10 +55,3 @@ function step() {
 }
 
 run();
-
-/*except KeyboardInterrupt:
-    cleanup()
-    exit( 1 )*/
-
-//cleanup()
-//exit( 0 )
