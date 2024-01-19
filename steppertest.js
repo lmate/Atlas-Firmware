90d71eabb7ebf129f078279370164e2132ad13be
Mate Lenkey <9825717+lmate@users.noreply.github.com>
2024-01-19 17:35:28 +0100

    .

--- steppertest.js
+++ steppertest.js
@@ -12,14 +12,14 @@ const step_count = 4096;
 const direction = false;
 
 const step_sequence = [
-[1, 0, 0, 1],
-[1, 0, 0, 0],
-[1, 1, 0, 0],
-[0, 1, 0, 0],
-[0, 1, 1, 0],
-[0, 0, 1, 0],
-[0, 0, 1, 1],
-[0, 0, 0, 1]];
+  [1, 0, 0, 1],
+  [1, 0, 0, 0],
+  [1, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [0, 0, 0, 1]];
 
 let motor_step_counter = 0;
 
@@ -34,24 +34,23 @@ function cleanup() {
   in4.unexport();
 }
 
-function run() {
-  for (let i = 0; i < step_count; i++) {
-    setTimeout(step, step_sleep * (i + 1));
+function run(steps) {
+
+  for (let i = 0; i < steps; i++) {
+    setTimeout(() => {
+      in1.writeSync(step_sequence[Math.abs(motor_step_counter)][0]);
+      in2.writeSync(step_sequence[Math.abs(motor_step_counter)][1]);
+      in3.writeSync(step_sequence[Math.abs(motor_step_counter)][2]);
+      in4.writeSync(step_sequence[Math.abs(motor_step_counter)][3]);
+
+      if (direction) {
+        motor_step_counter = (motor_step_counter - 1) % 8;
+      } else {
+        motor_step_counter = (motor_step_counter + 1) % 8
+      }
+    }, step_sleep * (i + 1));
   }
-  setTimeout(cleanup, step_sleep * (step_count + 1));
+  setTimeout(cleanup, step_sleep * (steps + 1));
 }
 
-function step() {
-  in1.writeSync(step_sequence[Math.abs(motor_step_counter)][0]);
-  in2.writeSync(step_sequence[Math.abs(motor_step_counter)][1]);
-  in3.writeSync(step_sequence[Math.abs(motor_step_counter)][2]);
-  in4.writeSync(step_sequence[Math.abs(motor_step_counter)][3]);
-
-  if (direction) {
-    motor_step_counter = (motor_step_counter - 1) % 8;
-  } else {
-    motor_step_counter = (motor_step_counter + 1) % 8
-  }
-}
-
-run();
+run(4096);
