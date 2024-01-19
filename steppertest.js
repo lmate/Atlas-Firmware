76a88e588e136a24a089c94580d40517ed909ed7
Mate Lenkey <9825717+lmate@users.noreply.github.com>
2024-01-19 17:48:20 +0100

    .

--- steppertest.js
+++ steppertest.js
@@ -1,15 +1,6 @@
 import { Gpio } from 'onoff';
 
-const in1 = new Gpio(17, 'out');
-const in2 = new Gpio(18, 'out');
-const in3 = new Gpio(27, 'out');
-const in4 = new Gpio(22, 'out');
-
-const step_sleep = 1;
-
-const step_count = 4096;
-
-const direction = false;
+const motorPins = [new Gpio(17, 'out'), new Gpio(18, 'out'), new Gpio(27, 'out'), new Gpio(22, 'out')];
 
 const step_sequence = [
   [1, 0, 0, 1],
@@ -21,7 +12,6 @@ const step_sequence = [
   [0, 0, 1, 1],
   [0, 0, 0, 1]];
 
-let motor_step_counter = 0;
 
 function cleanup() {
   in1.writeSync(0);
@@ -34,23 +24,26 @@ function cleanup() {
   in4.unexport();
 }
 
-function run(steps) {
+function run(steps, direction, speed) {
+  let sequence_step_counter = direction == 'CCW' ? steps : 0;
 
   for (let i = 0; i < steps; i++) {
-    setTimeout(() => {
-      in1.writeSync(step_sequence[Math.abs(motor_step_counter)][0]);
-      in2.writeSync(step_sequence[Math.abs(motor_step_counter)][1]);
-      in3.writeSync(step_sequence[Math.abs(motor_step_counter)][2]);
-      in4.writeSync(step_sequence[Math.abs(motor_step_counter)][3]);
 
-      if (direction) {
-        motor_step_counter = (motor_step_counter - 1) % 8;
-      } else {
-        motor_step_counter = (motor_step_counter + 1) % 8
+    setTimeout(() => {
+      for (let pinID = 0; pinID < motorPins.length; pinID++) {
+        motorPins[pinID].writeSync(step_sequence[sequence_step_counter][pinID]);
       }
-    }, step_sleep * (i + 1));
+
+      if (direction == 'CW') {
+        sequence_step_counter = (sequence_step_counter + 1) % 8;
+      } else if (direction == 'CCW') {
+        sequence_step_counter = (sequence_step_counter - 1) % 8
+      }
+
+    }, speed * (i + 1));
+    
   }
-  setTimeout(cleanup, step_sleep * (steps + 1));
+  setTimeout(cleanup, speed * (steps + 1));
 }
 
-run(4096);
+run(4096, 'CW', 1);
